b09ac331b953b47e89b10403630e306cfab3f51f
FranzCSUF <fsalvador79@gmail.com>
2018-05-31 22:57:48 -0700

    Define a function that renders a single character.(#1)

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
 var phrase = 'Grumpy wizards make toxic brew for the evil queen and jack'
 var space = ''
 var characters = phrase.split(space)
+var point = null
+var newObjects = toObjects()
+var $span = document.getElementById('verbiage')
+
+function toObjects() {
+  var array = []
+  for (var i = 0; i < characters.length; i++) {
+    var newObject = {
+      letter: characters[i]
+    }
+    array.push(newObject)
+  }
+  return array
+}
+
+function single(char) {
+  var text = document.createTextNode(char)
+  $span.appendChild(text)
+}
