47012ff8da1b1e2baae8632ce61ba5be22d05f51
Franz <fsalvadoro79@gmail.com>
2018-06-06 12:35:41 -0700

    Completed corrections for define a function that renders all characters and appends them to the page.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 var quote = 'Music produces a kind of pleasure which human nature cannot do without.'
-var characters = quote.split('')
-var spanChar = document.createElement('span')
+var arrObj = toObjects(quote)
+var $challenge = document.getElementById('challenge')
 
-function toObjects() {
+function toObjects(characters) {
   var arrayObj = []
   for (var i = 0; i < characters.length; i++) {
     var newObject = {
@@ -18,6 +18,20 @@ function single(input) {
   var output = 0
   for (var key in input) {
     output = input[key]
+    var spanChar = document.createElement('span')
     spanChar.textContent = output // return a span?
+    return spanChar
   }
 }
+// Loop through character objects
+// For each of them
+//  Create a new span
+//  Append the new span to the page
+function showChars(input) {
+  for (var i = 0; i < input.length; i++) {
+    var runSingle = single(input[i])
+    $challenge.appendChild(runSingle)
+  }
+}
+
+showChars(arrObj)
