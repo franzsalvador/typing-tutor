9ceeb449c33d988183913f6fb07c143a27f3dfb5
FranzCSUF <fsalvador79@gmail.com>
2018-06-07 13:09:36 -0700

    Revised compare the pressed key with the current character. Record failures to match the current character.(#3)

--- main.js
+++ main.js
@@ -13,36 +13,33 @@ function toObjects(characters) {
   }
   return arrayObj
 }
-// Take one object at a time
+
 function single(input, index, current) {
   var spanChar = document.createElement('span')
   if (index === current) {
     spanChar.className = 'currentChar'
   }
-  spanChar.textContent = input.letter //
+  spanChar.textContent = input.letter
   return spanChar
 }
-// Loop through character objects
-// For each of them
-//  Create a new span
-//  Append the new span to the page
+
 function showChars(characters) {
   for (var i = 0; i < characters.length; i++) {
     var runSingle = single(characters[i], i, appState.currentCharacter)
     $challenge.appendChild(runSingle)
   }
 }
-// Create an Object to represent the application state, and make the characters a property.
-// Add a property to the application state to track the index of the currentCharacter.
-// Define a CSS class to highlight the current-character.
-// Enhance function that renders a character to conditionally add a special current-character class.
+
 var appState = {
   characters: arrObj,
   currentCharacter: 0
 }
 
-document.addEventListener('keydown', function () {
+window.addEventListener('keydown', function (event) {
   $challenge.textContent = ''
+  if (event.key === 'Shift') {
+    return
+  }
   if (event.key !== appState.characters[appState.currentCharacter].letter) {
     appState.characters[appState.currentCharacter].failures++
   }
